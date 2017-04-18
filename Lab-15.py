e76d303d42878694c990df22eb20eb1643bd03fc
Steven <Steven.c.hunt@gmail.com>
2017-04-18 12:25:21 -0700

    Game: Craps (Dice)

--- Lab-15.py
+++ Lab-15.py
@@ -43,31 +43,5 @@ def craps():
         showInformation("You hit your point, you win!")
         printNow("You hit your point, you win!")
         Game = false
-
-# ----------------------------------------------------------------------------
-
-import calendar
-from datetime import *
-
-# Birthday: 05.21.1987
-
-def birthDate(): 
-
-  # 1:
-  birthDay = 21
-  birthMonth = 05
-  birthYear = 1987
-  
-  # 2:
-  nextBday = date(2017,birthMonth,birthDay)
-  today = date.today()
-  
-  # 3: 
-  DOI = date(1776,07,04)
-  
-  # Display: 
-  print '\n',calendar.month(birthYear,birthMonth)
-  print 'Next birthday is ', (nextBday - today), 'away.'
-  print 'The Declaration of Independence was ratified by the Continental Congress on a', calendar.day_name[DOI.weekday()], '.'
   
 
